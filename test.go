69a50c524442548a7d34545eacd27a3db640a837
alexquar <alexanderquarrie@gmail.com>
2025-04-25 16:49:24 -0400

    notes

---
+++ test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+func square(x int) int {
+	return x * x
+}
+
+func main() {
+	fmt.Print("Hello, World!\n")
+	test := []int{1, 2, 3, 4, 5}
+	sort.Ints(test)
+	thing := "thing"
+	fmt.Println(len(thing))
+	f, err := os.Open("gos.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err.Error())
+		return
+	}
+	defer f.Close()
+	fmt.Println("File opened successfully")
+}
