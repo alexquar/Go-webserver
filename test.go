221ba44a24f17fdb21aaa19322d0951b1bdc1c47
alexquar <alexanderquarrie@gmail.com>
2025-04-26 09:20:12 -0400

    concerency

--- test.go
+++ test.go
@@ -1,26 +1,26 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"os"
-	"sort"
-)
+// import (
+// 	"fmt"
+// 	"os"
+// 	"sort"
+// )
 
-func square(x int) int {
-	return x * x
-}
+// func square(x int) int {
+// 	return x * x
+// }
 
-func main() {
-	fmt.Print("Hello, World!\n")
-	test := []int{1, 2, 3, 4, 5}
-	sort.Ints(test)
-	thing := "thing"
-	fmt.Println(len(thing))
-	f, err := os.Open("gos.txt")
-	if err != nil {
-		fmt.Println("Error opening file:", err.Error())
-		return
-	}
-	defer f.Close()
-	fmt.Println("File opened successfully")
-}
+// func main() {
+// 	fmt.Print("Hello, World!\n")
+// 	test := []int{1, 2, 3, 4, 5}
+// 	sort.Ints(test)
+// 	thing := "thing"
+// 	fmt.Println(len(thing))
+// 	f, err := os.Open("gos.txt")
+// 	if err != nil {
+// 		fmt.Println("Error opening file:", err.Error())
+// 		return
+// 	}
+// 	defer f.Close()
+// 	fmt.Println("File opened successfully")
+// }
